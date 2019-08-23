0623226ae85d6c600889de65da10344a4a58867d
karandon24 <50190973+karandon24@users.noreply.github.com>
2019-08-23 20:36:06 +0530

    Update Android.mk

--- musix/Android.mk
+++ musix/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
- include $(CLEAR_VARS) LOCAL_MODULE := musix
+ include $(CLEAR_VARS) 
+LOCAL_MODULE := musix
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := prebuild/musix.apk
 LOCAL_CERTIFICATE := PRESIGNED
